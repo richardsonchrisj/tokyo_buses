57133e07f468eb5dfac000c3382ac32119632d14
Chris <richardsonchrisj@gmail.com>
2021-05-28 01:08:43 -0400

    updating

--- tokyo.js
+++ tokyo.js
@@ -1,25 +1,22 @@
-mapboxgl.accessToken = `${{secret.MAPBOX_KEY}}`;
+mapboxgl.accessToken = "pk.eyJ1IjoicmljaGFyZHNvbmNocmlzaiIsImEiOiJja29ub3RpcDAwMzAwMnJvaDhrMmo0cjdxIn0.wbl9nYD0s0NUEHv9cBEs2A";
 
 // This is the map instance of Japan Rail centered on Tokyo
 let map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/mapbox/dark-v10', // style URL
     center: [139.76842272980838, 35.680995983317494], // starting position [lng, lat]
-    zoom: 14 // starting zoom
+    zoom: 10 // starting zoom
 });
-
-//list of buses in Tokyo
-let busList = []
 
 //run once to get buses
 function getBuses() {
-    fetchPromise = fetch(`https://api-tokyochallenge.odpt.org/api/v4/%22odpt:Bus?acl:consumerKey=${{secret.TOKYO_KEY}}`);
-
+    const fetchPromise = fetch("https://api-tokyochallenge.odpt.org/api/v4/odpt:Bus?acl:consumerKey=RIwJ83sfSn8q_F_xBUkSVDScHYotKP8BQKT_0gT6EyM")
     fetchPromise.then(response => {
         return response.json();
     }).then(buses => {
+        console.log(buses.length)
 
-        for (let i = 0; i < buses.length; i++) {
+        for (let i = 0; i < 100; i++) {
 
             let busNumber = buses[i]["odpt:busNumber"]
             console.log("busNumber = " + buses[i]["odpt:busNumber"]);
@@ -40,12 +37,11 @@ function getBuses() {
                     .addTo(map);
             }
 
-            busList.push(busNumber);
         }
     })
 }
 
-
-getBuses();
-
-// let run = setInterval(getPositions, 5000);
+let run = setInterval(() => {
+    console.log(map);
+    getBuses()
+}, 5000);
